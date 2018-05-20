d72c33df2064a339d1c58eb0216e21a7f76bbb86
League <League@administorsimac.attlocal.net>
2018-05-20 15:58:23 -0700

    BinaryConverter

--- src/BinaryConverter.java
+++ src/BinaryConverter.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -28,7 +29,7 @@ void setup() {
 	panel.add(button);
 	button.setText("convert");
 	button.addActionListener(this);
-	frame.pack();
+	frame.pack();	 
 	
 }
 
@@ -37,6 +38,26 @@ public void actionPerformed(ActionEvent e) {
 	// TODO Auto-generated method stub
 	System.out.println("fdghj");
 	
+}
+
+String convert(String input) {
+    if(input.length() != 8){
+         JOptionPane.showMessageDialog(null, "Enter 8 bits, silly!!!");
+         return "-";
+    }
+    String binary = "[0-1]+";    //must contain numbers in the given range
+    if (!input.matches(binary)) {
+         JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s, silly!!!");
+         return "-";
+    }
+    try {
+         int asciiValue = Integer.parseInt(input, 2);
+         char theLetter = (char) asciiValue;
+         return "" + theLetter;
+    } catch (Exception e) {
+         JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
+         return "-";
+    }
 }
 
 }
